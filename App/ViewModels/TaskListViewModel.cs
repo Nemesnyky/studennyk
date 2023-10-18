130608e45a29a4d7685090a7d7cb9bc6dca160b1
Andrii Olkhovych <the.def4alt@gmail.com>
2023-10-18 19:53:51 +0200

    Add TaskListViewModel

---
+++ App/ViewModels/TaskListViewModel.cs
@@ -0,0 +1,42 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public class TaskListViewModel : INotifyPropertyChanged
+{
+    private TaskController taskController = new TaskController();
+    private ObservableCollection<Task> tasks;
+
+    public ObservableCollection<Task> Tasks
+    {
+        get { return tasks; }
+        set
+        {
+            tasks = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public TaskListViewModel()
+    {
+        Tasks = new ObservableCollection<Task>();
+    }
+
+    public async void LoadTasks(int userId)
+    {
+        var taskList = await System.Threading.Tasks.Task.Run(() => taskController.GetTaskList(userId));
+
+        Tasks.Clear();
+        foreach (var task in taskList)
+        {
+            Tasks.Add(task);
+        }
+    }
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
