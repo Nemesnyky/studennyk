9817833a72e471eb17dcf721866229b0e7057aeb
Yurii Kovalchuk <urakovalchuk3535@gmail.com>
2023-11-19 22:12:12 +0200

    Add AppLoggerProvider

---
+++ App/Services/AppLoggerProvider.cs
@@ -0,0 +1,17 @@
+﻿using MetroLog.MicrosoftExtensions;
+using Microsoft.Extensions.Logging;
+
+namespace App.Services
+{
+    public static class AppLoggerProvider
+    {
+        public static ILoggerFactory LoggerFactory { get; } =
+            Microsoft.Extensions.Logging.LoggerFactory.Create(builder =>
+        {
+            builder.AddTraceLogger(_ => { });
+        });
+
+        public static ILogger CreateLogger<T>() =>
+            LoggerFactory.CreateLogger<T>();
+    }
+}
