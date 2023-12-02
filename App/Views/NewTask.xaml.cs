502deb1dcb4acbf282de0607b6f307e595ddee08
Anna Zlobina <anna.zlobina1010@gmail.com>
2023-12-02 13:02:18 +0200

    Get atributes from entries and DateTimePickers

--- App/Views/NewTask.xaml.cs
+++ App/Views/NewTask.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,36 @@ namespace App.Views;
 
 public partial class NewTask : ContentView
 {
+    public DateTime newTaskDate;
+    public TimeSpan newTaskTime;
     public NewTask()
     {
         InitializeComponent();
         BindingContext = new NewTaskViewModel();
+
+    }
+    void DescriptionTextChanged(object sender, TextChangedEventArgs e)
+    {
+        string oldTextDescription = e.OldTextValue;
+        string newTextDescription = e.NewTextValue;
+        string textDescription = entryDescription.Text;
+    }
+    void TitleTextChanged(object sender, TextChangedEventArgs e)
+    {
+        string oldTextTitle = e.OldTextValue;
+        string newTextTitle = e.NewTextValue;
+        string textTitle = entryTitle.Text;
+    }
+    private void NewTaskDatePicker(object sender, DateChangedEventArgs e)
+    {
+        newTaskDate = newTaskDatePicker.Date;
+    }
+    void NewTaskTimePicker(object sender, PropertyChangedEventArgs args)
+    {
+        newTaskTime = newTaskTimePicker.Time;
+    }
+    void RewriteNewTask()
+    {
 
     }
 }
