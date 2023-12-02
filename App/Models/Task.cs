916c9e09f3f914392cf592e04c384b38243abcbf
PomalinaMargo <145930584+PomalinaMargo@users.noreply.github.com>
2023-12-02 19:15:05 +0200

    .......

--- App/Models/Task.cs
+++ App/Models/Task.cs
@@ -1,8 +1,13 @@
+using System;
+using System.Collections.Generic;
+
 namespace App.Models
 {
     public class Task
     {
         public static readonly long DEFAULT_ID = -1;
+        public const bool NOT_DONE = false;
+
         public long Id { get; set; } = DEFAULT_ID;
         public string Title { get; set; }
         public string Description { get; set; }
@@ -10,9 +15,7 @@ namespace App.Models
         public DateTimeOffset Due { get; set; }
         public bool IsDone { get; set; }
 
-        /// <summary>
-        /// Use DEFAULT_ID for Id if it is not known.
-        /// </summary>
+
         public Task(long id, string title, string description, DateTimeOffset created, DateTimeOffset due, bool isDone)
         {
             Id = id;
@@ -40,4 +43,50 @@ namespace App.Models
         }
     }
 
+    public class TaskFactory
+    {
+        private long lastId = 0;
+        private readonly Random random = new Random();
+
+        private const string DefaultTitle = "Title";
+        private const string DefaultDescription = "Description";
+        private static readonly DateTimeOffset DefaultCreated = DateTimeOffset.UtcNow;
+        private static readonly DateTimeOffset DefaultDue = DateTimeOffset.UtcNow.AddDays(7);
+
+        private const bool DefaultIsDone = Task.NOT_DONE;
+
+        public Task Generate(
+            string title = DefaultTitle,
+            string description = DefaultDescription,
+            DateTimeOffset created = default,
+            DateTimeOffset due = default,
+            bool isDone = DefaultIsDone)
+        {
+            var taskId = random.Next(1, 100) + lastId;
+            lastId = taskId;
+
+            Task task = new Task(taskId, title, description,
+                                created == default ? DefaultCreated : created,
+                                due == default ? DefaultDue : due,
+                                isDone);
+
+            return task;
+        }
+
+        public List<Task> GenerateMany(int numberOfTasks,
+                                       string title = DefaultTitle,
+                                       string description = DefaultDescription,
+                                       DateTimeOffset created = default,
+                                       DateTimeOffset due = default,
+                                       bool isDone = DefaultIsDone)
+        {
+            List<Task> tasks = new List<Task>();
+            for (int i = 0; i < numberOfTasks; i++)
+            {
+                Task task = Generate(title, description, created, due, isDone);
+                tasks.Add(task);
+            }
+            return tasks;
+        }
+    }
 }
